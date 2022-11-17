c1d78a0bbc6460e7a37553e55d9b74375aad5565
eonist <30n1st@gmail.com>
2022-11-17 19:21:15 +0100

    Update WizardHelper+Import+IOS.swift

--- Sources/WizardHelper/WizardHelper+Import+IOS.swift
+++ Sources/WizardHelper/WizardHelper+Import+IOS.swift
@@ -4,12 +4,15 @@ import MobileCoreServices
 import UniformTypeIdentifiers
 
 extension WizardHelper {
+   public typealias OnOpenResult = Result<[URL], Error>
+   public typealias OnOpenComplete = (_ result: OnOpenResult) -> Void
+   public static let defaultOnOpenComplete: OnOpenComplete = { result in Swift.print("on default complete result: \(result)") }
    /**
     * Open file
-    * - Note: ref: https://stackoverflow.com/a/48851508/5389500
+    * - Note: Ref: https://stackoverflow.com/a/48851508/5389500
     * - Note: iOS 14: https://stackoverflow.com/a/42370660/5389500
-    * - Note: apple docs: https://developer.apple.com/documentation/uikit/uidocumentpickerviewcontroller and https://developer.apple.com/documentation/uikit/view_controllers/adding_a_document_browser_to_your_app/presenting_selected_documents
-    * - Note: all files: "public.data"
+    * - Note: Apple docs: https://developer.apple.com/documentation/uikit/uidocumentpickerviewcontroller and https://developer.apple.com/documentation/uikit/view_controllers/adding_a_document_browser_to_your_app/presenting_selected_documents
+    * - Note: All files: "public.data"
     * - Remark: Seems like we cant make this sync like macOS ref: https://stackoverflow.com/a/40521834/5389500
     * - Note: Some claim it still possible with a counter and notification etc: https://stackoverflow.com/a/48659562/5389500
     * - Fixme: ⚠️️ 👉 Add the UTType code, it's iOS 14 only 👈
@@ -18,29 +21,27 @@ extension WizardHelper {
     *   - types: - Fixme: ⚠️️
     *   - complete: - Fixme: ⚠️️
     */
-   public static func promptOpenFile(view: UIView? = nil, types: [UTType] = defaultTypes, complete: @escaping (_ urls: [URL]) -> Void) {
+   public static func promptOpenFile(view: UIView? = nil, types: [UTType] = defaultTypes, complete: @escaping OnOpenComplete) {
       guard let view = view ?? UIViewController.topMostController()?.view else { Swift.print("Err, ⚠️️ unable to get view"); return }
-      //      Swift.print("promptOpenFile")
+      // Swift.print("promptOpenFile")
       let controller = OpenFileVC.init(forOpeningContentTypes: types)//.init(documentTypes: types, in: .import) // choose your desired documents the user is allowed to select, choose your desired UIDocumentPickerMode
       controller.delegate = controller // let documentPickerController = UIDocumentPickerViewController(forOpeningContentTypes: types)  //      let types = UTType.types(tag: "json", tagClass: UTTagClass.filenameExtension, conformingTo: nil)
-      //      controller.directoryURL?.startAccessingSecurityScopedResource()
       controller.onComplete = complete
-      //      controller.modalPresentationStyle = .overFullScreen
+      // controller.modalPresentationStyle = .overFullScreen
       // controller.modalPresentationStyle = .formSheet
       // controller.allowsMultipleSelection = false
       // controller.shouldShowFileExtensions = true
-      // - Fixme: ⚠️️ Probably use the topMostVC call and drop view param etc
       guard let vc = UIView.firstAvailableUIViewController(fromResponder: view) else { fatalError("ViewController not reachable") }
       vc.present(controller, animated: true) { Swift.print("WizardHelper - prompt completed presenting") }
    }
 }
 /**
- * - Fixme: ⚠️️ add doc
+ * - Fixme: ⚠️️ Add doc
  */
 private class OpenFileVC: UIDocumentPickerViewController, UIDocumentPickerDelegate {
-   // - Fixme: ⚠️️ make urls optional to account for cancel
-   var onComplete: (_ urls: [URL]) -> Void = { _ in Swift.print("on default complete")}
-   //   var urls: [URL] = []
+   // - Fixme: ⚠️️ Make URLs optional to account for cancel
+   var onComplete: WizardHelper.OnOpenComplete = WizardHelper.defaultOnOpenComplete //(_ urls: [URL]) -> Void = { _ in Swift.print("on default complete")}
+   // var urls: [URL] = []
    /**
     * - Fixme: ⚠️️ doc
     * - Parameters:
@@ -50,7 +51,7 @@ private class OpenFileVC: UIDocumentPickerViewController, UIDocumentPickerDelega
    func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
       // self.urls = urls // do something with the selected documents
       Swift.print("OpenFileVC - didPickDocumentsAt")
-      onComplete(urls)
+      onComplete(.success(urls))
    }
    /**
     * - Fixme: ⚠️️ doc
@@ -61,7 +62,7 @@ private class OpenFileVC: UIDocumentPickerViewController, UIDocumentPickerDelega
    func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentAt url: URL) {
       // self.urls = [url]  // do something with the selected document
       Swift.print("OpenFileVC - didPickDocumentAt")
-      onComplete([url])
+      onComplete(.success([url]))
    }
    /**
     * - Fixme: ⚠️️
@@ -70,7 +71,7 @@ private class OpenFileVC: UIDocumentPickerViewController, UIDocumentPickerDelega
    func documentPickerWasCancelled(_ controller: UIDocumentPickerViewController) {
       Swift.print("OpenFileVC - documentPickerWasCancelled")
       self.dismiss(animated: true, completion: nil)
-      onComplete([])
+      onComplete(.failure(NSError(domain: "User canceled", code: 0)))
    }
 }
 /**
@@ -81,7 +82,7 @@ extension WizardHelper {
     * - Fixme: ⚠️️ move to const ext
     */
    public static let defaultTypes: [UTType] = {
-      [UTType.json, UTType.text, UTType.zip] // "public.json"
+      [.json, .text, .zip] // "public.json"
    }()
 }
 #endif
