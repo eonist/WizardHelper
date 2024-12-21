84036bbac77495566d9b8fd6540554aa18e98a6a
eonist <30n1st@gmail.com>
2024-12-22 00:35:08 +0100

    Minor cleanup

--- Sources/WizardHelper/import/WizardHelper+Import+Mac.swift
+++ Sources/WizardHelper/import/WizardHelper+Import+Mac.swift
@@ -10,7 +10,8 @@ extension WizardHelper {
     * Prompts the user to select a file to open using an `NSOpenPanel`.
     * - Returns: The path to the selected file, or `nil` if the user cancelled the operation.
     * - Note: This method is only available on macOS.
-    * - Fixme: ⚠️️ Add more documentation for the `NSOpenPanel` instance and its methods.
+    * - Description: This method uses an `NSOpenPanel` to present a file selection dialog to the user. The user can navigate through the file system and select a file. If a file is selected, the method returns the file path as a string. If the user cancels the operation, the method returns `nil`.
+    * - Important: Ensure that the application has the necessary permissions to access the file system, as this method interacts with the user's file system.
     * - Remark: You have an extension for `NSSavePanel` in `WinExtension`: See `NSSavePanel.initialize...`.
     * ## Examples:
     * if let filePath = WizardHelper.promptOpenFile() {
